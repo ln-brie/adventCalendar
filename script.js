bfef74f2818e1d6d29fdeb4bcd435cfccbc3530f
Helene <helenebrie@yahoo.fr>
2020-11-10 15:29:18 +0100

    script style des jours indispos et affichage du p lors du clic

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var currentDate = new Date() ;
+    $("div.jour").each(function(index, val){
+        if (($(this).children("h1").text() > currentDate.getDate())/* || (currentDate.getMonth() !== 11)*/) {
+            $(this).css({
+                "color": "grey",
+                "background-color": "rgba(121, 120, 120, 0.151)",
+                "cursor": "default"
+            });
+        } else {
+            $(this).click(function() {
+                $(this).children('p').css("display", "block");
+            })
+        }
+
+    })
+    
+});
